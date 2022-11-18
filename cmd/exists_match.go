c00a5d38ad86c3822c2c949aefa8770bb2cf57b9
alecompte <github@leca.sh>
2022-11-18 18:56:37 -0500

    remove unused code branchs

--- cmd/exists_match.go
+++ cmd/exists_match.go
@@ -30,41 +30,14 @@ func doc_exists(dbName string, colName string, doc_id string, client *mongo.Clie
 }
 
 func doc_exists_and_match(dbName string, colName string, doc bson.D, client *mongo.Client) (bool, bool) {
-	/** if reflect.TypeOf(doc["_id"]).String() == "primitive.M" {
-		fmt.Println("Using slow scan")
-		res, err := client.Database(dbName).Collection(colName).Find(context.TODO(), bson.M{})
-		if err != nil {
-			fmt.Println(err)
-		}
-		for res.Next(context.TODO()) {
-			var elem bson.M
-			err := res.Decode(&elem)
-			if err != nil {
-				fmt.Println(err)
-			}
-			for k, v := range doc["_id"].(bson.M) {
-				if elem["_id"].(bson.M)[k] != v {
-					fmt.Println("match")
-					return true, false
-				}
-			}
-
-			if reflect.DeepEqual(doc["_id"].(bson.M), elem["_id"].(bson.M)) {
-				return true, true
-			} else {
-				return true, false
-			}
-		}
-	} else { **/
-		q := bson.D{{"_id", doc.Map()["_id"]}}
-		
-		res := client.Database(dbName).Collection(colName).FindOne(context.TODO(), q)
-		if res.Err() != nil {
-			return false, false
-		} else {
-			var result bson.D
-			res.Decode(&result)
-			return true, reflect.DeepEqual(doc.Map(), result.Map())
-		}
-	//}
+	q := bson.D{{"_id", doc.Map()["_id"]}}
+	
+	res := client.Database(dbName).Collection(colName).FindOne(context.TODO(), q)
+	if res.Err() != nil {
+		return false, false
+	} else {
+		var result bson.D
+		res.Decode(&result)
+		return true, reflect.DeepEqual(doc.Map(), result.Map())
+	}
 }
